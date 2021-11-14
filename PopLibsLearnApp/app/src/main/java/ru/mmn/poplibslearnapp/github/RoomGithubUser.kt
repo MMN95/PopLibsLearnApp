3ba4cab55264f49ad1944edd6a2529e344996b33
Michael Makulov <mmn95@mail.ru>
2021-11-14 21:52:37 +0500

    -Реализация кэширования данных с помощью Room

---
+++ PopLibsLearnApp/app/src/main/java/ru/mmn/poplibslearnapp/github/RoomGithubUser.kt
@@ -0,0 +1,13 @@
+package ru.mmn.poplibslearnapp.github
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class RoomGithubUser(
+    @PrimaryKey var id: String,
+    var login: String,
+    var avatarUrl: String,
+    var reposUrl: String
+)
+
